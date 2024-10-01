e668730a9cb3daa841298516ade708788a62da8a
Towardsyou <Towardsyou@users.noreply.github.com>
2024-10-01 21:00:11 +0200

    env_logger and tracing span, before 4.5.5

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,13 @@
 use actix_web::{HttpResponse, Responder};
+use uuid::Uuid;
 
 pub async fn health_check() -> impl Responder {
+    let request_id = Uuid::new_v4();
+
+    let request_span =
+        tracing::info_span!("health check", %request_id, test_key="key", test_value="value");
+    let _request_span_guard = request_span.enter();
+    tracing::info!("before health check");
+    tracing::info!("after health check");
     HttpResponse::Ok()
 }
--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::{types::Uuid, PgPool};
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormSubscrib {
@@ -8,10 +9,16 @@ pub struct FormSubscrib {
     email: String,
 }
 
-pub async fn subscribe(
-    form: web::Form<FormSubscrib>,
-    pool: web::Data<PgPool>,
-) -> impl Responder {
+pub async fn subscribe(form: web::Form<FormSubscrib>, pool: web::Data<PgPool>) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+    "Adding a new subscriber.",
+    %request_id,
+    subscriber_email = %form.email,
+    subscriber_name= %form.name
+    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -25,9 +32,16 @@ pub async fn subscribe(
     // We use `get_ref` to get an immutable reference to the `PgConnection`
     // wrapped by `web::Data`.
     .execute(pool.get_ref())
-    .await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish()
+    .instrument(query_span)
+    .await
+    {
+        Ok(_) => {
+            tracing::info!("new subscription inserted");
+            HttpResponse::Ok().finish()
+        }
+        Err(e) => {
+            tracing::error!("error inserting new subscription {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
     }
-
 }
