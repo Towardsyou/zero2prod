49928601bf5485dd88826dafbb6270cb1f54ce55
Towardsyou <Towardsyou@users.noreply.github.com>
2024-10-05 15:01:32 +0200

    unit test for subscriber email, before 6.14

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -25,32 +25,56 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::*;
 
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+
     #[test]
     fn valid_email_is_accepted() {
         let result = SubscriberEmail::from_str(" ursula@domain.com");
         assert!(result.is_ok());
+
+        let email: String = SafeEmail().fake();
+        claim::assert_ok!(SubscriberEmail::from_str(&email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn randomly_generated_email_is_accepted(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::from_str(&valid_email.0).is_ok()
     }
 
     #[test]
-    fn empty_string_is_rejected(){
+    fn empty_string_is_rejected() {
         let result = SubscriberEmail::from_str("");
         assert!(result.is_err());
     }
 
     #[test]
-    fn email_missing_at_symbol_is_rejected(){
+    fn email_missing_at_symbol_is_rejected() {
         let result = SubscriberEmail::from_str("example.com");
         assert!(result.is_err());
     }
 
     #[test]
-    fn email_empty_left_of_at_symbol_is_rejected(){
+    fn email_empty_left_of_at_symbol_is_rejected() {
         let result = SubscriberEmail::from_str("@example.com");
         assert!(result.is_err());
     }
 
     #[test]
-    fn email_empty_right_of_at_symbol_is_rejected(){
+    fn email_empty_right_of_at_symbol_is_rejected() {
         let result = SubscriberEmail::from_str("example@");
         assert!(result.is_err());
     }
