65f23c9df1fb8a5e45914306cb0e9a8b6d98aedc
Towardsyou <Towardsyou@users.noreply.github.com>
2024-10-15 00:06:56 +0200

    before 9.3

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,9 @@
 mod health_check;
 mod subscription;
 mod subscription_confirm;
+mod newsletters;
 
 pub use health_check::*;
 pub use subscription::*;
 pub use subscription_confirm::*;
+pub use newsletters::*;
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -2,3 +2,4 @@ mod health_check;
 mod helper;
 mod subscription;
 mod subscription_confirm;
+mod newsletter;
